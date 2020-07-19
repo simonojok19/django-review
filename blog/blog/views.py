1d8330db64100c08a1f90e57b34b49502bbf7c82
simonojok19 <simonojok19@gmail.com>
2020-07-19 19:45:46 +0300

    fixed some bug

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -8,11 +8,11 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, year, month, day, post):
-    post = get_list_or_404(Post, slug=post,
-                           status='published',
-                           publish__year=year,
-                           pubish__month=month,
-                           publish__day=day)
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=slug,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
