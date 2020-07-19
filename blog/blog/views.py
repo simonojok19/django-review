4912730eb4b27c5daf1533e89fd1eb7826b59754
simonojok19 <simonojok19@gmail.com>
2020-07-19 17:42:26 +0300

    added two controllers

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -6,3 +6,13 @@ from .models import Post
 def post_list(request):
     posts = Post.publish.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_list_or_404(Post, slug=post,
+                           status='published',
+                           publish__year=year,
+                           pubish__month=month,
+                           publish__day=day)
+
+    return render(request, 'blog/post/detail.html', {'post': post})
