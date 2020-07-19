04245c7f0ce4ebe96e86626cf88039815230386e
simonojok19 <simonojok19@gmail.com>
2020-07-19 17:34:07 +0300

    added some codes

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Post
+
 
 # Create your views here.
+def post_list(request):
+    posts = Post.publish.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
